5598f4f51b93f68dfaa0f827e7db71d7e7d57f9e
Rohit Arya <rohit1447481@gmail.com>
2025-02-15 22:10:13 +0530

    Update Utils.js

--- client/src/Utils/Utils.js
+++ client/src/Utils/Utils.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API=axios.create({baseURL:"http://localhost:8000"})
+const API=axios.create({baseURL:"https://todo-list-beckend.vercel.app/"})
 
 export {axios,API}
